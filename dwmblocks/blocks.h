238b32733b75365f3e77f5411926927dafeb7195
ScathTheFloof <skylineprotofen@gmail.com>
2024-11-07 20:21:51 -0600

    stopped using acpi for dwmblocks battery status

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"[VOL: ", "block-volume.sh", 1, 10}, 
-	{"[BAT: ", "acpi -b | awk '{print $4}' | sed 's/.$/]/'", 1, 0},
+	{"[BAT: ", "cat /sys/class/power_supply/BAT0/capacity | awk '{print $1\"%]\"}'", 1, 0},
 	{"[CPU: ", "top -b -n1 | grep \"Cpu(s)\" | awk '{print $2 + $4 \"%]\"}'", 1, 0},
 	{"[RAM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 \"]\"}' | sed s/i//g",	1,		0},
 
